bd34068658a5b3a6fc01d43c5992b1d4134c3efc
Librarian423 <youg4231998@naver.com>
2022-12-22 17:50:26 +0900

    arrays

---
+++ ConsoleApp5/ConsoleApp5/MyArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+
+namespace Question
+{
+    class MainApp
+    {
+        //1번
+        //(1)번 int 배열에 string 값을 할당
+        //
+
+        //2번
+        private static void MatrixMul(int[,] a, int[,] b)
+		{
+            int[,] array = new int[2, 2];
+            int temp = 0;
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+                    temp = 0;
+					for (int k = 0; k < 2; k++)
+					{
+                        temp += a[i, k] * b[k, j];
+                        
+					}
+                    array[i, j] = temp;
+                }
+			}
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"[{i}, {j}] : {array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //3번
+        //5 4 3 2 1
+
+        //4번
+        //1 2 3 4 5 
+
+        //5번
+        private static void HTable()
+		{
+            Hashtable ht = new Hashtable();
+            ht["회사"] = "Microsoft";
+            ht["URL"] = "www.microsoft.com";
+
+            Console.WriteLine("회사 : {0}", ht["회사"]);
+            Console.WriteLine("URL : {0}", ht["URL"]);
+        }
+        static void Main(string[] args)
+        {
+            //2번
+            int[,] matrixA =new int[2,2] { { 3, 2 }, { 1, 4 } };
+            int[,] matrixB = new int[2, 2] { { 9, 2 }, { 1, 7 } };
+            MatrixMul(matrixA, matrixB);
+            Console.WriteLine();
+            HTable();
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace ConsoleApp6
+{
+    public class MyArray
+    {
+        public static void Reverse(int[] array, int index, int length)
+        {
+            int destIndex = index + length;
+            int startIndex = index;
+            if (array == null  )
+			{
+                Console.WriteLine("ArgumentNullException");
+                return;
+            }
+            if (destIndex > array.Length ||
+                startIndex >= array.Length ||
+                startIndex < 0 ||
+                length < 0 ||
+                length >= array.Length) 
+			{
+                Console.WriteLine("ArgumentOutOfRangeException");
+                return;
+            }
+            if (array.Rank > 1) 
+			{
+                Console.WriteLine("RankException");
+                return;
+            }
+            
+			for (int i = 0; i < length/2; i++)
+			{
+                int temp = array[startIndex + i];
+                array[startIndex + i] = array[destIndex - i-1];
+                array[destIndex - i - 1] = temp;
+
+            }
+        }
+
+        public static void Fill(int[] array, int value, int startIndex, int count)
+        {
+            int destIndex = startIndex + count;
+            //int index = startIndex;
+            if (array == null)
+            {
+                Console.WriteLine("ArgumentNullException");
+                return;
+            }
+            if (destIndex > array.Length ||
+                startIndex >= array.Length ||
+                startIndex < 0 ||
+                count < 0 ||
+                count >= array.Length)
+            {
+                Console.WriteLine("ArgumentOutOfRangeException");
+                return;
+            }
+            if (array.Rank > 1)
+            {
+                Console.WriteLine("RankException");
+                return;
+            }
+
+            for (int i = startIndex; i < destIndex; i++)
+            {
+                array[i] = value;
+            }
+        }
+
+        public static void Copy(int[] sourceArray, int[] destinationArray, int length)
+        {
+
+        }
+
+        public static void Resize(ref int[] array, int newSize)
+        {
+        }
+
+        public static void Clear(int[] array)
+        {
+
+        }
+
+        public static void Clear(int[] array, int index, int length)
+        {
+
+        }
+
+        public static void Sort(int[] array)
+        {
+        }
+
+        public static int IndexOf(int[] array, int value)
+        {
+            return 0;
+        }
+
+        public static int BinarySearch(int[] array, int value)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+
+            Reverse(array, 0, 4);
+            foreach (var item in array)
+            {
+                Console.WriteLine("{0}", item);
+            }
+            Fill(array, 1, 1, 3);
+			foreach (var item in array)
+			{
+                Console.WriteLine("{0}", item);
+			}
+           
+        }
+    }
+    
+}
